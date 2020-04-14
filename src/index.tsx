d8fde2cfedc2a6ffeeed17bee04c3933f4af9736
孙航 <2662419405@qq.com>
2020-04-14 16:11:55 +0800

    修改导入文件和版本

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import "./styles/index.scss";
+import "./index.css";
 library.add(fas);
 
 export { default as Button } from "./components/Button";
