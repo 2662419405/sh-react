64a9adc06591856dfff1016aee6562cbffc7b472
孙航 <2662419405@qq.com>
2020-04-14 13:39:43 +0800

    :100: 完善文档和组间设计

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,67 +1,73 @@
-import React from "react";
-import classnames from "classnames";
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import classNames from 'classnames'
 
-export enum ButtonSize {
-  Large = "lg",
-  Small = "sm",
-}
-
-export enum ButtonType {
-  Primary = "primary",
-  Default = "default",
-  Danger = "danger",
-  Link = "link",
-}
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
   className?: string;
-  size?: ButtonSize;
-  btnType?: ButtonType;
+  /**设置 Button 的禁用 */
   disabled?: boolean;
+  /**设置 Button 的尺寸 */
+  size?: ButtonSize;
+  /**设置 Button 的类型 */
+  btnType?: ButtonType;
   children: React.ReactNode;
   href?: string;
 }
-
-type NativeButtonProps = BaseButtonProps &
-  React.ButtonHTMLAttributes<HTMLElement>;
-type AnchorButtonProps = BaseButtonProps &
-  React.AnchorHTMLAttributes<HTMLElement>;
-
-export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
-
-const Button: React.FC<ButtonProps> = (props) => {
-  const {
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互
+ * ### 引用方法
+ * 
+ * ~~~js
+ * import { Button } from 'vikingship'
+ * ~~~
+ */
+export const Button: FC<ButtonProps> = (props) => {
+  const { 
     btnType,
+    className,
     disabled,
     size,
     children,
     href,
-    className,
     ...restProps
-  } = props;
-  const classes = classnames("btn", className, {
+  } = props
+  // btn, btn-lg, btn-primary
+  const classes = classNames('btn', className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    disabled: btnType === ButtonType.Link && disabled,
-  });
-  if (btnType === ButtonType.Link && href) {
+    'disabled': (btnType === 'link') && disabled
+  })
+  if (btnType === 'link' && href ) {
     return (
-      <a {...restProps} href={href} className={classes}>
+      <a
+        className={classes}
+        href={href}
+        {...restProps}
+      >
         {children}
       </a>
-    );
+    )
   } else {
     return (
-      <button {...restProps} className={classes} disabled={disabled}>
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >
         {children}
       </button>
-    );
+    )
   }
-};
+}
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default,
-};
+  btnType: 'default'
+}
 
 export default Button;
