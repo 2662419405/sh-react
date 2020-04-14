4b20f3de2360a71b734673032180e52e7215f020
孙航 <2662419405@qq.com>
2020-04-14 14:23:51 +0800

    修改文件配置:100:

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -5,11 +5,11 @@ storiesOf('Welcome page', module)
   .add('welcome', () => {
     return (
       <>
-        <h1>欢迎来到 vikingship 组件库</h1>
-        <p>vikingship 是为慕课网课程打造的一套教学组件库，从零到一让大家去学习</p>
+        <h1>欢迎来到 sh-react 组件库</h1>
+        <p>sh-react 是模仿antd库去设计和练习的一款UI库,针对于React的用户</p>
         <h3>安装试试</h3>
         <code>
-          npm install vikingship --save
+          npm install sh-react --save
         </code>
       </>
     )
